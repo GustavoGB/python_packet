ff218731dc1fc248ce2dffcd6197b8208be5687e
Gustavo Gobetti Bertocco <gustavogb@al.insper.edu.br>
2021-10-14 21:28:26 -0300

    Finalizado

--- scripts/hello.py
+++ scripts/hello.py
@@ -2,16 +2,14 @@ from dev_aberto import hello
 import babel.dates
 import gettext
 import datetime
-
-
 gettext.install('cli',localedir='locale')
 
 if __name__ == '__main__':
-    
+
     date, name = hello()
 
-    #formated_date = datetime.strptime(date, '%Y-%m-%dT')
-    
-    formated_date = babel.dates.format_datetime(date)
+    formated_date = datetime.datetime.strptime(date, '%Y-%m-%dT%H:%M:%S%z') 
+
+    formated_date = babel.dates.format_datetime(formated_date)
 
     print(_('Último commit feito em:'), formated_date, _(' por'), name)
